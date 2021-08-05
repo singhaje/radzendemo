ffa39335c96a065c2fff350581325489ffb0c346
Ajeet Singh <singhaje@gmail.com>
2021-08-05 18:22:17 +0000

    commit

---
+++ server/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RadzenAzureAuth
+{
+    public partial class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            var redirectUrl = Url.Content("~/");
+
+            return Challenge(new AuthenticationProperties { RedirectUri = redirectUrl }, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        public IActionResult Logout()
+        {
+            var redirectUrl = Url.Content("~/");
+
+            return SignOut(new AuthenticationProperties { RedirectUri = redirectUrl }, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
